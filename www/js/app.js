202aa16f1393d68b4e0a9d877c43bc3df34e2fae
Kin <kinsz89@gmail.com>
2015-12-03 00:35:54 -0500

    Second commit

---
+++ www/js/app.js
@@ -0,0 +1,49 @@
+angular.module('ionicApp', ['ionic'])
+
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+  $ionicConfigProvider.tabs.position('bottom');
+  $ionicConfigProvider.navBar.alignTitle('center');
+  })
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+$stateProvider
+  .state('BedroomSize', {
+      url: '/Bedroom-Size',
+      templateUrl: 'templates/BedroomSize.html',
+      controller: 'BedroomSizeCtrl'
+  })
+  .state('AddDoors', {
+      url: '/Add-Doors',
+      templateUrl: 'templates/AddDoors.html',
+      controller: 'AddDoorsCtrl'
+  })
+  .state('AddWindows', {
+      url: '/Add-Windows',
+      templateUrl: 'templates/addwindows.html',
+      controller: 'AddWindowsCtrl'
+  })    
+  .state('AddFurniture', {
+      url: '/Add-Furniture',
+      templateUrl: 'templates/AddFurniture.html',
+      controller: 'AddFurnitureCtrl'
+  })  
+    .state('Layout', {
+      url: '/Layout',
+      templateUrl: 'templates/Layout.html',
+      controller: 'LayoutCtrl'
+  })  
+  $urlRouterProvider.otherwise('/Bedroom-Size');
+})  
+
+.controller('BedroomSizeCtrl', function($scope) {
+})
+.controller('AddDoorsCtrl', function($scope) {
+})
+.controller('AddWindowsCtrl', function($scope) {
+})
+.controller('AddFurnitureCtrl', function($scope) {
+})
+.controller('LayoutCtrl', function($scope) {
+})
+
